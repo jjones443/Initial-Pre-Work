39761c3e372f46772f0a520f93e51c736c965e93
jjones443 <jalenjones443@gmail.com>
2019-03-31 12:58:15 -0500

    Completed task Declare JavaScript Variables

---
+++ 5-Introduction-to-JavaScript/2-Declare-JavaScript-Variables.js
@@ -0,0 +1,7 @@
+// In this lesson I learned to use variables and how they are essentially a label used to point to a specific data value
+
+// Example
+var ourName;
+
+// Declare myName below this line
+var myName;
