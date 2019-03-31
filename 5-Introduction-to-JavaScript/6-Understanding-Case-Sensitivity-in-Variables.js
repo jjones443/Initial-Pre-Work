a73602ebcd7e78767a547964adc1d76a9efbb926
jjones443 <jalenjones443@gmail.com>
2019-03-31 16:42:17 -0500

    Completed task Understanding Case Sensitivity in Variables

---
+++ 5-Introduction-to-JavaScript/6-Understanding-Case-Sensitivity-in-Variables.js
@@ -0,0 +1,11 @@
+// In this lesson I learned to use camel case when using variables to prevent consistency errors 
+
+// Declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
