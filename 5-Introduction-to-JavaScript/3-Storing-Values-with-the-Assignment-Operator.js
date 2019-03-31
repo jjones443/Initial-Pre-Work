fa5ace4e3061c41f6a874cda2b545a0411ee851d
jjones443 <jalenjones443@gmail.com>
2019-03-31 13:07:11 -0500

    Completed task Storing Values with the Assignment Operator

---
+++ 5-Introduction-to-JavaScript/3-Storing-Values-with-the-Assignment-Operator.js
@@ -0,0 +1,9 @@
+// In this lesson I learned that assigning a value to the variable is the data that the specific variable would display after it has been set
+
+// Setup
+var a;
+var b = 2;
+
+// Only change code below this line
+a = 7;
+b = a;
