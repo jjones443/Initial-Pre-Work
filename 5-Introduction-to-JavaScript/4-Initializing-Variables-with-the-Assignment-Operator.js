9bda502f9c1a826cbf9e318e2e17099ba8455b79
jjones443 <jalenjones443@gmail.com>
2019-03-31 13:09:44 -0500

    Completed task Initializing Variables with the Assignment Operator

---
+++ 5-Introduction-to-JavaScript/4-Initializing-Variables-with-the-Assignment-Operator.js
@@ -0,0 +1,8 @@
+// In this lesson I learned that variables can be set in line with the variable declaration, saving time and space in the code
+
+// Example
+var ourVar = 19;
+
+// Only change code below this line
+
+var a = 9;
