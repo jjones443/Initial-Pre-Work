a6a56438aef3473df2258c7f105f52d92451fe8a
jjones443 <jalenjones443@gmail.com>
2019-04-02 17:27:31 -0500

    Completed task Word Blanks

---
+++ 5-Introduction-to-JavaScript/35-Word_Blanks.js
@@ -0,0 +1,12 @@
+// In this lesson I learned to use the + concatenation symbol inside the "" symbols to add the different variables to my string
+
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  // Your code below this line
+  var result = "The black "+myNoun+" was very "+myAdjective+" and it "+myVerb+" very "+myAdverb+"";
+
+  // Your code above this line
+  return result;
+}
+
+// Change the words here to test your function
+wordBlanks("car", "sleek", "drove", "fast");
