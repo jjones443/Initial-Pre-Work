3f01cac470386a9983059002c9772df53ff98692
jjones443 <jalenjones443@gmail.com>
2019-04-01 21:08:14 -0500

    Completed task Escape Sequences in Strings

---
+++ 5-Introduction-to-JavaScript/24-Escape-Sequences-in-Strings.js
@@ -0,0 +1,3 @@
+// In this lesson I learned to use the backslash \ for more than just escaping quotes. It can be used to escape things like placing text on a new line or to tab text forward without the specific commands showing in the output
+
+var myStr = "FirstLine\n\t\SecondLine\nThirdLine"; // Change this line
