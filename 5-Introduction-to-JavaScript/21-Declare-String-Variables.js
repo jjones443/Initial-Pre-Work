a5c6b20406e4dc9df7bb7041b0bc7bcb4f97cf76
jjones443 <jalenjones443@gmail.com>
2019-04-01 20:51:13 -0500

    Completed task Declare String Variables

---
+++ 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -0,0 +1,10 @@
+// In this lesson I learned that the characters inside of the "" or '' are called string literals
+
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+
+var myFirstName = "Jalen"
+var myLastName = "Jones"
