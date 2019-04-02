009e52ff3bb0ef477418c72e1657fefca6b5c193
jjones443 <jalenjones443@gmail.com>
2019-04-02 18:29:40 -0500

    Completed task Write Reusable JavaScript with Functions

---
+++ 5-Introduction-to-JavaScript/46-Write_Reusable_JavaScript_with_Functions.js
@@ -0,0 +1,17 @@
+/* In this lesson I learned to use functions which allow the code listed within the function to be displayed
+whenever it is called or invoked */
+
+// Example
+function ourReusableFunction() {
+  console.log("Heyya, World");
+}
+
+ourReusableFunction();
+
+// Only change code below this line
+
+function reusableFunction() {
+    console.log("Hi World");
+}
+
+reusableFunction();
