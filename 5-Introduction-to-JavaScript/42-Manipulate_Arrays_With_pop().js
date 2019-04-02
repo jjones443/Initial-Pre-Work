4f1df095fea0b15bb30b1102ffddcd7da5a7d8dd
jjones443 <jalenjones443@gmail.com>
2019-04-02 18:16:22 -0500

    Completed Manipulate Arrays With pop

---
+++ 5-Introduction-to-JavaScript/42-Manipulate_Arrays_With_pop().js
@@ -0,0 +1,12 @@
+// In this lesson I learned that the pop function can remove an item from an array and assign it to a new variable
+
+// Example
+var ourArray = [1,2,3];
+var removedFromOurArray = ourArray.pop(); 
+// removedFromOurArray now equals 3, and ourArray now equals [1,2]
+
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line.
+var removedFromMyArray = myArray.pop(["cat", 2]);
