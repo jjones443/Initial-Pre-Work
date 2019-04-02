064eaf2605637475d420315102ba49ef9cde3510
jjones443 <jalenjones443@gmail.com>
2019-04-02 17:10:45 -0500

    Completed task Use Bracket Notation to Find the Nth to Last Character in a String

---
+++ 5-Introduction-to-JavaScript/34-Use_Bracket_Notation_to_Find_the_Nth-to-Last_Character_in_a_String.js
@@ -0,0 +1,14 @@
+// In this lesson I applied the lesson learned in the previous lesson to find other characters in relation to the last
+/* The resolution for second to last has me choosing index 2 which to my understanding of index numbers is that 2 would
+equal the character a in Lovelace but the second to last character is actually c. I am not sure if 
+I am misunderstanding the lesson or what second to last means */
+
+// Example
+var firstName = "Ada";
+var thirdToLastLetterOfFirstName = firstName[firstName.length - 3];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var secondToLastLetterOfLastName = lastName[lastName.length - 2];
