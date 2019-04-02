bfbdcedc5548c2f0e3c452d94619e3bdfc24ba64
jjones443 <jalenjones443@gmail.com>
2019-04-01 20:43:42 -0500

    Completed task Compound Assignment With Augmented Addition

---
+++ 5-Introduction-to-JavaScript/17-Compound-Assignment-With-Augmented-Addition.js
@@ -0,0 +1,11 @@
+// In this lesson I learned to combine an operation and assignment into one operator 
+
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only modify code below this line
+
+a += 12;
+b += 9;
+c += 7;
