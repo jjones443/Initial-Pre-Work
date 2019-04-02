aab889bbeb87e1ab5bbcf58d6d406028221a539d
jjones443 <jalenjones443@gmail.com>
2019-04-01 21:13:03 -0500

    Completed task Concatenating Strings with the Plus Equals Operator

---
+++ 5-Introduction-to-JavaScript/26-Concatenating_Strings_with_the_Plus_Equals_Operator.js
@@ -0,0 +1,10 @@
+// In this lesson I learned you can add on strings to existing strings using +=
+
+// Example
+var ourStr = "I come first. ";
+ourStr += "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the first sentence. " ;
+myStr += "This is the second sentence.";
