80201bdf84e17f2efe3bffc6526b9e25be8ea6e8
jjones443 <jalenjones443@gmail.com>
2019-04-02 16:45:41 -0500

    Completed task Appending Variables to Strings

---
+++ 5-Introduction-to-JavaScript/28-Appending_Variables_to_Strings.js
@@ -0,0 +1,12 @@
+// In this lesson I learned to concatenate two variables together to create a string
+
+// Example
+var anAdjective = "awesome!";
+var ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
+
+// Only change code below this line
+
+var someAdjective = "fun!"
+var myStr = "Learning to code is ";
+myStr += someAdjective;
