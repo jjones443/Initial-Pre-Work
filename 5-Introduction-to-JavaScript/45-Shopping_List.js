f5e2cb0006edda1af1fe988acb7a1929b6c29de9
jjones443 <jalenjones443@gmail.com>
2019-04-02 18:25:32 -0500

    Completed task Shopping List

---
+++ 5-Introduction-to-JavaScript/45-Shopping_List.js
@@ -0,0 +1,5 @@
+/* In this lesson I learned to make a multi-dimensional array shopping list where each 
+array includes the item as a string and the amount of items needed as a number */
+
+var myList = [["milk", 2],["butter", 4],["eggs", 12],["flour", 1],["sugar", 1]];
+
