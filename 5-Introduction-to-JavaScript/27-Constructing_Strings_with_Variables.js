770129fe1cc6aaf84f4ea66b884d48966ba16417
jjones443 <jalenjones443@gmail.com>
2019-04-02 16:41:46 -0500

    Completed task Constructing Strings with Variables

---
+++ 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -0,0 +1,9 @@
+// In this lesson I learned that you can use variables in addition to strings to create a statement
+
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "Jalen";
+var myStr = "My name is " + myName + " and I am well!"
