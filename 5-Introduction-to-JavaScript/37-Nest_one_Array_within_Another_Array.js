ba4da8b23f31b4c26b0624a3f6b9215deebf2343
jjones443 <jalenjones443@gmail.com>
2019-04-02 17:56:35 -0500

    Completed task Nest on Array within Another Array

---
+++ 5-Introduction-to-JavaScript/37-Nest_one_Array_within_Another_Array.js
@@ -0,0 +1,7 @@
+// In this lesson I learned you can put arrays into other arrays
+
+// Example
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+// Only change code below this line.
+var myArray = [["the galaxy", 12345], ["all", 100]];
