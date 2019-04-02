c38d5a2b3b33363017d7a689d6efb9e316cb3408
jjones443 <jalenjones443@gmail.com>
2019-04-01 20:57:50 -0500

    Completed task Escaping Literal Quotes in Strings

---
+++ 5-Introduction-to-JavaScript/22-Escaping-Literal-Quotes-in-Strings.js
@@ -0,0 +1,3 @@
+// In this lesson I learned that you can use the backslash symbol \ to prevent JavaScript from thinking a quote in text is the end of a string. This is done by putting \" \' when using quotes in text.
+
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
