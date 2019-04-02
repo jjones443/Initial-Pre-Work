e5b68cdae80d30f178db031e485fa632e3538d84
jjones443 <jalenjones443@gmail.com>
2019-04-01 21:02:42 -0500

    Completed task Quoting Strings with Single Quotes

---
+++ 5-Introduction-to-JavaScript/23-Quoting-Strings-with-Single-Quotes.js
@@ -0,0 +1,3 @@
+// In this lesson I learned that using single '' and double "" quotes within the same string is allowed. It would create different variables in the string if the start and end quotes are the same
+
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
