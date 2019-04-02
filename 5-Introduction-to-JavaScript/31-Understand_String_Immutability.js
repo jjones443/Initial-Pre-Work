12ff7ec76e498a47d4b35b85f2af163ceff4672a
jjones443 <jalenjones443@gmail.com>
2019-04-02 16:57:52 -0500

    Completed task Understand String Immutability

---
+++ 5-Introduction-to-JavaScript/31-Understand_String_Immutability.js
@@ -0,0 +1,9 @@
+// In this lesson I learned that strings cannot be modified once they have been set. The way around this is to reassign the string with the corrected information
+
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+
+myStr = "Hello World"; // Fix Me
+
