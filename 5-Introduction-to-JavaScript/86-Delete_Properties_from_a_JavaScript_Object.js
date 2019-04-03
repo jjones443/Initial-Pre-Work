101e468c6f80028733d082b9c2102a4672253ec0
jjones443 <jalenjones443@gmail.com>
2019-04-03 18:14:15 -0500

    Completed task Delete Properties from a JavaScript Object

---
+++ 5-Introduction-to-JavaScript/86-Delete_Properties_from_a_JavaScript_Object.js
@@ -0,0 +1,25 @@
+// In this lesson I learned to delete properties. either dot or bracket notation work with this 
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"],
+  "bark": "bow-wow"
+};
+
+delete ourDog.bark;
+
+// Setup
+var myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
+
+// Only change code below this line.
+
+delete myDog["tails"];
