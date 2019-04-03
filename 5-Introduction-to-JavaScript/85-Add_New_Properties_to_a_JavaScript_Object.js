9fc2ece612c37fdaa4a105ba99d5db5c6356232d
jjones443 <jalenjones443@gmail.com>
2019-04-03 18:12:05 -0500

    Completed task Add New Properties to a JavaScript Object

---
+++ 5-Introduction-to-JavaScript/85-Add_New_Properties_to_a_JavaScript_Object.js
@@ -0,0 +1,22 @@
+// In this lesson I learned to add new properties using dot or bracket notation. I opted for bracket in this lesson
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+ourDog.bark = "bow-wow";
+
+// Setup
+var myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line.
+myDog["bark"] = "woof";
