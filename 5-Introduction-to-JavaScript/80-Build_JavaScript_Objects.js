ab7bbdef3b60c205aa9c89a8169edf535ef0ee11
jjones443 <jalenjones443@gmail.com>
2019-04-03 13:54:28 -0500

    Completed task Build JavaScript Objects

---
+++ 5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,20 @@
+// In this lesson I learned to use objects in javascript which use properties to describe what can be a real world object
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  
+  "name": "Bruce",
+  "legs": 3,
+  tails: 1,
+  "friends": ["Robin", 7]
+  
+};
