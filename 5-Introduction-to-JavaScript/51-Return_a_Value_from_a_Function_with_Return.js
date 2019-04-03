fef7ff14b7259b1da00ee0261698ee2c13a8eb44
jjones443 <jalenjones443@gmail.com>
2019-04-02 19:00:28 -0500

    Completed task Return a Value from a Function with Return

---
+++ 5-Introduction-to-JavaScript/51-Return_a_Value_from_a_Function_with_Return.js
@@ -0,0 +1,15 @@
+/* In this lesson I learned that a function can be used to return a value by assigning an argument to the funtion as
+well as a set value within the function */
+
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+// Only change code below this line
+
+function timesFive(num) {
+  return num * 5;
+}
+
+console.log(timesFive(10));
