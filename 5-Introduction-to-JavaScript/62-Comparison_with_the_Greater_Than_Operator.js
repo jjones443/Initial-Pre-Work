4cb7385b60c55f708d3c760482460a306b4759cc
jjones443 <jalenjones443@gmail.com>
2019-04-02 21:36:00 -0500

    Completed task Comparison with the Greater Than Operator

---
+++ 5-Introduction-to-JavaScript/62-Comparison_with_the_Greater_Than_Operator.js
@@ -0,0 +1,16 @@
+// In this lesson I learned to use the greater than operator
+
+function testGreaterThan(val) {
+  if (val > 100) {  // Change this line
+    return "Over 100";
+  }
+  
+  if (val > 10) {  // Change this line
+    return "Over 10";
+  }
+
+  return "10 or Under";
+}
+
+// Change this value to test
+testGreaterThan(10);
