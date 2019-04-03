246f2c3562b370aa0f1b2fab2959b74cf1fc6cc5
jjones443 <jalenjones443@gmail.com>
2019-04-03 16:43:20 -0500

    Completed task Accessing Object Properties with Bracket Notation

---
+++ 5-Introduction-to-JavaScript/82-Accessing_Object_Properties_with_Bracket_Notation.js
@@ -0,0 +1,14 @@
+/* In this lesson I learned that you use bracket notation when searching for object properties with two names
+but can also be used for single name properties */
+
+// Setup
+var testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+
+var entreeValue = testObj ["an entree"];   // Change this line
+var drinkValue = testObj ["the drink"];    // Change this line
