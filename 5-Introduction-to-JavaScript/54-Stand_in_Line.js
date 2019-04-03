e0a304fa21f75558aebc67b7df867065a1bbd0ad
jjones443 <jalenjones443@gmail.com>
2019-04-02 20:07:04 -0500

    Completed task Stand in Line

---
+++ 5-Introduction-to-JavaScript/54-Stand_in_Line.js
@@ -0,0 +1,16 @@
+// In this lesson I learned to add items to the back of a queue and remove items from the front
+
+function nextInLine(arr, item) {
+  // Your code here
+  arr.push(item);
+  var removed = arr.shift();
+  return removed;  // Change this line
+}
+
+// Test Setup
+var testArr = [1,2,3,4,5];
+
+// Display Code
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6)); // Modify this line to test
+console.log("After: " + JSON.stringify(testArr));
