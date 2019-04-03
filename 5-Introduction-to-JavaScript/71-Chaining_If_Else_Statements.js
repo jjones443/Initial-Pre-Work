8f3049d6229461a8633747234822120a5e0a36e3
jjones443 <jalenjones443@gmail.com>
2019-04-02 22:50:18 -0500

    Completed task Chaining If Else Statements

---
+++ 5-Introduction-to-JavaScript/71-Chaining_If_Else_Statements.js
@@ -0,0 +1,21 @@
+// In this lesson I learned to chain else if statements
+
+function testSize(num) {
+  // Only change code below this line
+  if (num < 5) {
+    return "Tiny"
+  } else if (num < 10) {
+    return "Small"
+  } else if (num < 15) {
+    return "Medium"
+  } else if (num < 20) {
+    return "Large"
+  } else if (num >= 20) {
+    return "Huge"
+  }
+  
+  // Only change code above this line
+}
+
+// Change this value to test
+testSize(7);
