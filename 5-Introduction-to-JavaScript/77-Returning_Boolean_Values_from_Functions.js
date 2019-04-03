aa1dc955f518e7b1ec02f22bce1b5843ba3ae24a
jjones443 <jalenjones443@gmail.com>
2019-04-02 23:26:16 -0500

    Completed task Returning Boolean Values from Functions

---
+++ 5-Introduction-to-JavaScript/77-Returning_Boolean_Values_from_Functions.js
@@ -0,0 +1,9 @@
+// In this lesson I learned to discover boolean values in an easier way than if/else statements
+
+function isLess(a, b) {
+  // Fix this code
+ return a < b;
+}
+
+// Change these values to test
+isLess(10, 15);
