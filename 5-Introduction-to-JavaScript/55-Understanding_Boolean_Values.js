f4ea831bb1716bdf5073bb66245f615d0cdad4d2
jjones443 <jalenjones443@gmail.com>
2019-04-02 20:25:32 -0500

    Completed task Understanding Boolean Values

---
+++ 5-Introduction-to-JavaScript/55-Understanding_Boolean_Values.js
@@ -0,0 +1,10 @@
+// In this lesson I learned boolean values are either true or false
+
+function welcomeToBooleans() {
+
+// Only change code below this line.
+
+return true; // Change this line
+
+// Only change code above this line.
+}
