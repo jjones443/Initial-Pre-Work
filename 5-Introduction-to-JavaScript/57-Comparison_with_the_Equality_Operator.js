f98a5b5e46b0a4a7b8ab163ea6d4e88bed40bccc
jjones443 <jalenjones443@gmail.com>
2019-04-02 21:25:30 -0500

    Completed task Comparison with the Equality Operator

---
+++ 5-Introduction-to-JavaScript/57-Comparison_with_the_Equality_Operator.js
@@ -0,0 +1,12 @@
+// In this lesson I learned to use the equality operator == to compare values of items using the boolean if condition
+
+// Setup
+function testEqual(val) {
+  if (val == 12) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+testEqual(10);
