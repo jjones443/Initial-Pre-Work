4aaa21bb03a863e2abd3f5ee19567bcd5075ec91
jjones443 <jalenjones443@gmail.com>
2019-04-03 17:53:51 -0500

    Completed task Accessing Object Properties with Variables

---
+++ 5-Introduction-to-JavaScript/83-Accessing_Object_Properties_with_Variables.js
@@ -0,0 +1,13 @@
+// In this lesson I learned to use a variable with brackets to locate a property within an object
+
+// Setup
+var testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line;
+
+var playerNumber = 16;       // Change this Line
+var player = testObj[playerNumber];   // Change this Line
