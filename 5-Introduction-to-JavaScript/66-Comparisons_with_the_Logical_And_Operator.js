fc4c7080ae4bef5c20f0a385952ff7c8e5ef6997
jjones443 <jalenjones443@gmail.com>
2019-04-02 21:45:58 -0500

    Completed task Comparisons with the Logical And Operator

---
+++ 5-Introduction-to-JavaScript/66-Comparisons_with_the_Logical_And_Operator.js
@@ -0,0 +1,16 @@
+// In this lesson I learned to combine multiple comparison operators within an if statement
+
+function testLogicalAnd(val) {
+  // Only change code below this line
+
+  if (val <= 50 && val >= 25) {
+      return "Yes";
+    }
+  
+
+  // Only change code above this line
+  return "No";
+}
+
+// Change this value to test
+testLogicalAnd(50);
