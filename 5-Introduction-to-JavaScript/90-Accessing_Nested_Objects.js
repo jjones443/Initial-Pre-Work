4ca19322766a2bbc2e48d0932206c6a9e52c8e84
jjones443 <jalenjones443@gmail.com>
2019-04-03 20:01:54 -0500

    Completed task Accessing Nested Objects

---
+++ 5-Introduction-to-JavaScript/90-Accessing_Nested_Objects.js
@@ -0,0 +1,16 @@
+// In this lesson I learned to find a result within a nested object using combined dot and bracket notation
+
+// Setup
+var myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+var gloveBoxContents = myStorage.car.inside["glove box"]; // Change this line
