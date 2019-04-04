81e9f58495b7b3d8bd1aed2ded025428cda67a54
jjones443 <jalenjones443@gmail.com>
2019-04-03 19:35:26 -0500

    Completed task Manipulating Complex Objects

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,26 @@
+// In this lesson I learned that multiple objects can be added to an array
+
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [ 
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  
+  {
+    "artist": "Tash Sultana",
+    "title": "Flow State",
+    "release_year": 2018,
+    "formats": [
+      "CD",
+      "Vinyl",
+      "Digital"
+    ]
+  }// Add record here
+];
