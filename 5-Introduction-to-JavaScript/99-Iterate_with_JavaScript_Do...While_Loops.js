95f815b674f7a0a8ed054dccdf4402ad9c02c4b2
jjones443 <jalenjones443@gmail.com>
2019-04-03 23:06:12 -0500

    Completed task Iterate with JavaScript Do...While Loops

---
+++ 5-Introduction-to-JavaScript/99-Iterate_with_JavaScript_Do...While_Loops.js
@@ -0,0 +1,12 @@
+// In this lesson I learned that loops can be forced to go through the code at least once before failing with a do...while loop
+
+// Setup
+var myArray = [];
+var i = 10;
+
+// Only change code below this line.
+
+do {
+  myArray.push(i);
+  i++;
+} while (i < 5);
