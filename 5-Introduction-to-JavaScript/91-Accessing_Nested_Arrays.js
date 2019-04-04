7cdc2dc3056bc639763c3d0c4a6600690f070e57
jjones443 <jalenjones443@gmail.com>
2019-04-03 20:05:05 -0500

    Completed task Accessing Nested Arrays

---
+++ 5-Introduction-to-JavaScript/91-Accessing_Nested_Arrays.js
@@ -0,0 +1,25 @@
+// In this lesson I learned to use array bracket and dot notation to locate items within an array
+
+// Setup
+var myPlants = [
+  { 
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }  
+];
+
+// Only change code below this line
+
+var secondTree = myPlants[1].list[1]; // Change this line
