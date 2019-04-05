e99c22d85ec755e6a89718907822a7df998ec727
jjones443 <jalenjones443@gmail.com>
2019-04-04 19:07:56 -0500

    Completed task Generate Random Fractions with JavaScript

---
+++ 5-Introduction-to-JavaScript/101-Generate_Random_Fractions_with_JavaScript.js
@@ -0,0 +1,10 @@
+// In this lesson I learned to generate a random fraction using the Math.random() function
+
+function randomFraction() {
+
+  // Only change code below this line.
+
+  return Math.random();
+
+  // Only change code above this line.
+}
