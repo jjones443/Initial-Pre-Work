2782cacdd2aa13b398b6338c2bfa3a7dbee619e2
jjones443 <jalenjones443@gmail.com>
2019-04-04 19:29:51 -0500

    Completed task Use the parseInt Function

---
+++ 5-Introduction-to-JavaScript/104-Use_the_parseInt_Function.js
@@ -0,0 +1,8 @@
+// In this lesson I learned to use the parselInt to convert string numbers into integers
+
+function convertToInteger(str) {
+ 
+ return parseInt(str);
+}
+
+convertToInteger("James");
