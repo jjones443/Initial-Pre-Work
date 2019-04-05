ad9812de063e6de76733e3fb4d30715a61fb4658
jjones443 <jalenjones443@gmail.com>
2019-04-04 19:19:54 -0500

    Completed task Generate Random Whole Numbers within a Range

---
+++ 5-Introduction-to-JavaScript/103-Generate_Random_Whole_Numbers_within_a_Range.js
@@ -0,0 +1,20 @@
+// In this lesson I learned to generate numbers within a range using min and max placeholders
+
+// Example
+function ourRandomRange(ourMin, ourMax) {
+
+  return Math.floor(Math.random() * (ourMax - ourMin + 1)) + ourMin;
+}
+
+ourRandomRange(1, 9);
+
+// Only change code below this line.
+
+function randomRange(myMin, myMax) {
+
+  return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin; // Change this line
+
+}
+
+// Change these values to test your function
+var myRandom = randomRange(5, 15);
