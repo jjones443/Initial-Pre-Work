d2e23847965d0216ed01dcf15b51c83aa04c7724
jjones443 <jalenjones443@gmail.com>
2019-04-06 08:55:14 -0500

    Completed task Declare a Read-Only Variable with the const Keyword

---
+++ 6-Introduction-to-the-ES6-Challenges/3-Declare-a-Read-Only-Variable-with-the-const-Keyword.js
@@ -0,0 +1,16 @@
+// In this lesson I learned that the const keyword is read only and cannot be updated once set
+
+function printManyTimes(str) {
+  "use strict";
+
+  // change code below this line
+
+  const SENTENCE = str + " is cool!";
+  for(let i = 0; i < str.length; i+=2) {
+    console.log(SENTENCE);
+  }
+
+  // change code above this line
+
+}
+printManyTimes("freeCodeCamp");
