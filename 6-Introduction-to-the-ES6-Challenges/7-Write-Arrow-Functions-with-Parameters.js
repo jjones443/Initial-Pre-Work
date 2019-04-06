91c1b8753c98696b84cf990c685180dc05355477
jjones443 <jalenjones443@gmail.com>
2019-04-06 09:37:16 -0500

    Completed task Write Arrow Functions with Parameters

---
+++ 6-Introduction-to-the-ES6-Challenges/7-Write-Arrow-Functions-with-Parameters.js
@@ -0,0 +1,8 @@
+// In this lesson I learned to use the arrow function with parameter placeholders
+
+const myConcat = (arr1, arr2) => {
+  "use strict";
+  return arr1.concat();
+};
+// test your code
+console.log(myConcat([1, 2], [3, 4, 5]));
