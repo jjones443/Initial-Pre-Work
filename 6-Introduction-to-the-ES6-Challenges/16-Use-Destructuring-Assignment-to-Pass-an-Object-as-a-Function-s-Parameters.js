d4b89f023c627b7d812e07e2803a1cba5b99eef3
jjones443 <jalenjones443@gmail.com>
2019-04-07 11:16:02 -0500

    Completed task Use Destructuring Assignment to Pass an Object as a Function's Parameters

---
+++ 6-Introduction-to-the-ES6-Challenges/16-Use-Destructuring-Assignment-to-Pass-an-Object-as-a-Function-s-Parameters.js
@@ -0,0 +1,23 @@
+// In this lesson I learned to manipulate specific information from an object using the destructuring assignment
+
+const stats = {
+  max: 56.78,
+  standard_deviation: 4.34,
+  median: 34.54,
+  mode: 23.87,
+  min: -0.75,
+  average: 35.85
+};
+const half = (function() {
+  "use strict"; // do not change this line
+
+  // change code below this line
+    return function half({max, min}) {
+    return (max + min) / 2.0;
+  };
+      
+  // change code above this line
+
+})();
+console.log(stats); // should be object
+console.log(half(stats)); // should be 28.015
