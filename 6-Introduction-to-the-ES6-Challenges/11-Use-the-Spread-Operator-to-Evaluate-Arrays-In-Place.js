6d13b0cac0d6c7b4a8fa2099aa7bf5e5ca0e2e4d
jjones443 <jalenjones443@gmail.com>
2019-04-07 09:57:34 -0500

    Completed task Use the Spread Operator to Evaluate Arrays In-Place

---
+++ 6-Introduction-to-the-ES6-Challenges/11-Use-the-Spread-Operator-to-Evaluate-Arrays-In-Place.js
@@ -0,0 +1,9 @@
+// In this lesson I learned to use the ...arr operator to spread an array
+
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+(function() {
+  "use strict";
+  arr2 = [...arr1]; // change this line
+})();
+console.log(arr2);
