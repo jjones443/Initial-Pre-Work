f03a48dbc7a28faf4ba6ec47143ab58573373b94
jjones443 <jalenjones443@gmail.com>
2019-04-08 09:35:29 -0500

    Completed task Write Concise Declarative Functions with ES6

---
+++ 6-Introduction-to-the-ES6-Challenges/19-Write-Concise-Declarative-Functions-with-ES6.js
@@ -0,0 +1,13 @@
+// In this lesson I learned you can use functions without the specific function keyword and colon symbol
+
+// change code below this line
+const bicycle = {
+  gear: 2,
+  setGear(newGear) {
+    "use strict";
+    this.gear = newGear;
+  }
+};
+// change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
