108265776570f00ccb8257619c08b335109f5a5f
jjones443 <jalenjones443@gmail.com>
2019-04-08 09:58:46 -0500

    Completed task Understand the Differences Between import and require

---
+++ 6-Introduction-to-the-ES6-Challenges/22-Understand-the-Differences-Between-import-and-require.js
@@ -0,0 +1,5 @@
+// In this lesson I learned to use import instead of require to bring only desired functions
+
+"use strict";
+capitalizeString("hello!");
+import {capitalizeString} from "string_functions";
